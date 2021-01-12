8846ca94ddac87a6e75f9dfa0a975865c5265a31
battlesqui_d <25509915+Battlesquid@users.noreply.github.com>
2021-01-11 23:40:37 -0700

    why

--- events/message.js
+++ events/message.js
@@ -6,7 +6,7 @@ module.exports = async (bot, message) => {
     if (message.author.bot) return;
     if (!message.guild.available) return;
 
-    if (message.author.id === "423699849767288853" && (/meme/gi.test(message.channel.name) || message.channel.id === "409464383358631944")) {
+    if (message.author.id === "300842468482613259" && (/meme/gi.test(message.channel.name) || message.channel.id === "409464383358631944")) {
         message.react(emotes[Math.floor(Math.random() * emotes.length)])
     }
 
